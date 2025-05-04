411c7f6ff99a18dcbd29ab90993204712451ca9c
bmarron18 <soil.foodwebs@gmail.com>
2025-05-03 21:18:55 -0600

    03May2025

---
+++ UTECA_5A_Students_25-3/5A_LessonPlans/w07/HW_05.2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Your Name
+Class ID
+HW_05.2
+Date
+
+Code Snippets from W3 Python Tutorial
+"""
+
+This HW contains code snippets from the following W3 Schools Tutorials:
+    Python Syntax
+	Python Comments
+	Python Variables
+	Python Data Types
+
+The tutorials can be found at:
+    https://www.w3schools.com/python/
+
+# %%
+Python Syntax
+
+# %%
+print("Hello, World!")
+
+# %%
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+# %%
+
+
+
